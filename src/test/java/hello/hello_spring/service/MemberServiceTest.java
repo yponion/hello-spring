dcd360e7f28c500d65a722182aa9c4866a831ff0
양정운 <yp071704@gmail.com>
2026-01-12 22:44:30 +0900

    feat: [1-5-15] 컴포넌트 스캔과 자동 의존관계 설정

--- src/test/java/hello/hello_spring/service/MemberServiceTest.java
+++ src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -17,7 +17,7 @@ class MemberServiceTest {
     @BeforeEach
     public void beforeEach() {
         memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
+        memberService = new MemberService(memberRepository); 
     }
 
     @AfterEach
